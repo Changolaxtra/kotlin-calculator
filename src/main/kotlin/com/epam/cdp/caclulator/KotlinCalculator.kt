ffe300670b88146a08141436723538bd70343663
Daniel Rojas <v-darojas@expediagroup.com>
2022-05-31 12:06:16 -0500

    Data and Operators added

--- src/main/kotlin/com/epam/cdp/caclulator/KotlinCalculator.kt
+++ src/main/kotlin/com/epam/cdp/caclulator/KotlinCalculator.kt
@@ -1,6 +1,7 @@
 package com.epam.cdp.caclulator
 
 import com.epam.cdp.caclulator.converter.SuffixExpressionConverter
+import com.epam.cdp.caclulator.data.NumberString
 import java.util.Stack;
 import java.util.Optional;
 
@@ -8,23 +9,26 @@ class KotlinCalculator {
 
     fun calculate(input: String): String {
         val converter = SuffixExpressionConverter();
-        val stack: Stack<Double> = Stack()
+        val stack: Stack<NumberString> = Stack<NumberString>()
         val suffixExpression = converter.convert(input)
         Optional.ofNullable(suffixExpression)
-            .orElse("")
-            .toCharArray()
-            .forEach { item ->
-                when (item) {
-                    '+' -> stack.push(stack.pop() + stack.pop())
-                    '-' -> stack.push(stack.pop() - stack.pop())
-                    '/' -> stack.push(stack.pop() / stack.pop())
-                    '*' -> stack.push(stack.pop() * stack.pop())
-                    else -> {
-                        stack.push(item.toString().toDouble())
+                .orElse("")
+                .toCharArray()
+                .forEach { item ->
+                    when (item) {
+                        '+' -> stack.push(stack.pop() + stack.pop())
+                        '-' -> stack.push(stack.pop() - stack.pop())
+                        '/' -> stack.push(stack.pop() / stack.pop())
+                        '*' -> stack.push(stack.pop() * stack.pop())
+                        else -> {
+                            stack.push(NumberString(item.toString()))
+                        }
                     }
                 }
-            }
 
         return stack.pop().toString();
     }
+
+
+
 }
